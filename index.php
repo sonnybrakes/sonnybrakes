b339e5e112197a95dae04059f6874bb85c780800
Michel Frechette <mifrec@hotmail.com>
2018-05-14 08:58:29 -0400

    3-7-C use wp-function bloginfo() c/w an argument to retrieve particular data from the wp-admin dasboard General Settings

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-<?php
-  function greet($name, $colour) {
-    echo "<p>Hi, my name is $name and my favourite colour is $colour.</p>";
-  }
-  greet('John', 'blue');
-  greet('Jane', 'green');
- ?>
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
