2f3d9a40fabb5d403450153688f0d9c652ce7076
Michel Frechette <mifrec@hotmail.com>
2018-05-14 08:37:14 -0400

    3-7-A created and called my first php function

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-this is the first php file required when developing a custom theme
+<?php
+  function myFirstFunction() {
+    echo 2 + 2;
+    echo "<p>create and call my first php function</p>";
+  };
+  myFirstFunction();
+?>
