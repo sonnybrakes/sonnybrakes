056c8682793416036a8c1d5525f34e1c4d900df3
Michel Frechette <mifrec@hotmail.com>
2018-05-25 05:40:45 -0400

    3-8-C using a php array() function

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-  $names = array('mitch', 'carole', 'pauline', 'julie'); 
+  $names = array('mitch', 'carole', 'pauline', 'julie');
 
  ?>
+
+<p>Hi, my name is <?php echo $names[2]; ?>.</p>
