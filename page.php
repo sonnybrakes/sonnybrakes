5ff62b5bebd417c2c24019c117b02ba37560e686
Michel Frechette <mifrec@hotmail.com>
2018-05-30 05:26:51 -0400

    5-13-H comment out the .page-links div

--- page.php
+++ page.php
@@ -21,13 +21,13 @@
         <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
       </div>
 
-      <div class="page-links">
+      <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
-      </div>
+      </div> -->
 
       <div class="generic-content">
         <?php the_content(); ?>
